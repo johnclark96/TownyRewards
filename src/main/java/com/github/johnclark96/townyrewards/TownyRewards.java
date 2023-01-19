74b6b3fbedf72a14b08aa11fb8df618c835c45f0
JClark <mlitvinkox96@gmail.com>
2023-01-18 22:06:44 -0500

    Initial Commit

---
+++ src/main/java/com/github/johnclark96/townyrewards/TownyRewards.java
@@ -0,0 +1,17 @@
+package com.github.johnclark96.townyrewards;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownyRewards extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
